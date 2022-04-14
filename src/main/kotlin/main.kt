060874d2111cbc2deadf218fd81c3ed1d5ea374f
aluochvivian <aluochviviann@gmail.com>
2022-04-14 09:30:06 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,40 @@
+import jdk.jfr.Category
+
+fun main(){
+var L= CurrentAccount(12,"Nalenyi",450000)
+    println(L)
+    println()
+}
+ data class CurrentAccount(var accountnumber:Int,var accountname :String,var balance:Int)
+fun deposit(amount: Double){
+    var a= deposit(700000.50)
+    println()
+ fun widthraw(amount:Double){
+     var b=widthraw(250000.50)
+     println()
+  fun details(accountnumber:Int,balance:Int,name:String){
+      var statement=("account number"+" "+"balance"+" "+"name")
+         println(statement)
+     }
+
+
+ }
+data class Product(var name:String,var weight:Int,var price:Int,var category:String)
+fun Category(name:String)
+    var product = String
+    when product=String
+            println(name)
+    when product=weight
+            println(num)
+
+
+fun indices(names:Indices<String>):Indices<String>{
+    names.forEachIndexed{names->
+        z(2==0)
+        println(name)
+    }
+
+}
+
+
+
